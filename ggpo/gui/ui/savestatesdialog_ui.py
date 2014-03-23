0d8a5840f1a40876949492c43acc469f1ba3733b
doctorguile <doctorguile@gmail.com>
2014-03-23 00:31:44 -0700

    display game title etc info when selecting unsupported game savestates

---
+++ ggpo/gui/ui/savestatesdialog_ui.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ggpo/gui/ui/savestatesdialog.ui'
+#
+# Created: Sat Mar 22 23:42:41 2014
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_SavestatesDialog(object):
+    def setupUi(self, SavestatesDialog):
+        SavestatesDialog.setObjectName(_fromUtf8("SavestatesDialog"))
+        SavestatesDialog.resize(648, 771)
+        self.verticalLayout = QtGui.QVBoxLayout(SavestatesDialog)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.uiSavestatesTblv = QtGui.QTableView(SavestatesDialog)
+        self.uiSavestatesTblv.setObjectName(_fromUtf8("uiSavestatesTblv"))
+        self.verticalLayout.addWidget(self.uiSavestatesTblv)
+        self.uiButtonBox = QtGui.QDialogButtonBox(SavestatesDialog)
+        self.uiButtonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.uiButtonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.uiButtonBox.setObjectName(_fromUtf8("uiButtonBox"))
+        self.verticalLayout.addWidget(self.uiButtonBox)
+
+        self.retranslateUi(SavestatesDialog)
+        QtCore.QObject.connect(self.uiButtonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), SavestatesDialog.accept)
+        QtCore.QObject.connect(self.uiButtonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), SavestatesDialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(SavestatesDialog)
+
+    def retranslateUi(self, SavestatesDialog):
+        SavestatesDialog.setWindowTitle(QtGui.QApplication.translate("SavestatesDialog", "Unsupported game savestates", None, QtGui.QApplication.UnicodeUTF8))
+
